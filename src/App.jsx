dc290cb1bfc22cfa2d73fdd8d7e613a9d98de7fa
Conner-DeGraaf <connershark2016@gmail.com>
2025-12-05 17:21:43 -0500

    Refactor App component to use Circle
    Removed introductory text and added Circle component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-
-/// cd portfolio then npm run dev
+import Circle from "./components/Circle" // Main circle at home page
 
 function App() {
 
   return (
-    <div>
-      <h1>Conner DeGraaf</h1>
-      <p>Hello, my name is Conner, this is my first vite project. I will be making a portfolio.</p>
-    </div>
+    <Circle/>
   )
 }
 
